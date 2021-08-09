8dd346596d365d89c746dcbbfa0783059684239c
Mark Caguioa <mark.caguioa@swapoolabs.com>
2021-08-10 07:42:26 +0800

    WIP: adding countries and states on registration page

--- lib/feature/register/register_page.dart
+++ lib/feature/register/register_page.dart
@@ -1,17 +1,16 @@
+import 'dart:convert';
+
 import 'package:flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:signtracker/blocs/auth/authentication_bloc.dart';
 import 'package:signtracker/blocs/login/login_bloc.dart';
 import 'package:signtracker/blocs/login/login_state.dart';
-import 'package:signtracker/feature/dashboard/dashboard_page.dart';
 import 'package:signtracker/feature/login/login_page.dart';
 import 'package:signtracker/repository/user_repository.dart';
 import 'package:signtracker/styles/values/values.dart';
 import 'package:signtracker/utilities/color_helper.dart';
-import 'package:signtracker/utilities/token_helper.dart';
 import 'package:signtracker/widgets/rounded_button.dart';
 
 class RegisterPage extends StatefulWidget {
@@ -34,12 +33,24 @@ class _RegisterPageState extends State<RegisterPage> {
   // Initially password is obscure
   bool _obscureText = true;
 
-  String _password;
-
   // Toggles the password show status
   void _toggle() {
     setState(() {
       _obscureText = !_obscureText;
+    });
+  }
+
+  String countryState = "";
+  List countryStates = List.empty(growable: true);
+
+  String country = "";
+  List countries = List.empty(growable: true);
+
+  loadJson() async {
+    String data = await DefaultAssetBundle.of(context)
+        .loadString('assets/countries_regions.json');
+    setState(() {
+      countries = json.decode(data);
     });
   }
 
@@ -48,6 +59,10 @@ class _RegisterPageState extends State<RegisterPage> {
     bloc = LoginBloc(
         RepositoryProvider.of<AuthenticationBloc>(context), UserRepository());
     super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      await loadJson();
+    });
   }
 
   void onRegisterButtonPressed() {
@@ -92,6 +107,7 @@ class _RegisterPageState extends State<RegisterPage> {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       appBar: _LoginAppbar(),
+      backgroundColor: Colors.white,
       body: BlocConsumer<LoginBloc, LoginState>(
         bloc: bloc,
         listener: (context, state) {
@@ -133,7 +149,7 @@ class _RegisterPageState extends State<RegisterPage> {
                         child: Column(
                           children: [
                             Text(
-                              'You have succesfully created an account! \nPlease check the email you registered and verify your account before logging in.',
+                              'You have successfully created an account! \nPlease check the email you registered and verify your account before logging in.',
                               style: GoogleFonts.karla(
                                   fontSize: 16, fontStyle: FontStyle.normal),
                               textAlign: TextAlign.center,
@@ -167,7 +183,6 @@ class _RegisterPageState extends State<RegisterPage> {
         builder: (context, state) {
           return SingleChildScrollView(
             child: Container(
-              color: Colors.white,
               child: Column(
                 children: [
                   Padding(
@@ -175,7 +190,7 @@ class _RegisterPageState extends State<RegisterPage> {
                     child: TextField(
                       controller: usernameController,
                       enabled: !(state is LoginLoading),
-                      style: textTheme.body1.copyWith(
+                      style: textTheme.bodyText2.copyWith(
                         color: Colors.black45,
                       ),
                       keyboardType: TextInputType.emailAddress,
@@ -193,7 +208,7 @@ class _RegisterPageState extends State<RegisterPage> {
                           ),
                         ),
                         hintText: 'Email',
-                        hintStyle: textTheme.body1.copyWith(
+                        hintStyle: textTheme.bodyText2.copyWith(
                           color: Colors.black12,
                         ),
                       ),
@@ -206,7 +221,7 @@ class _RegisterPageState extends State<RegisterPage> {
                       controller: passwordController,
                       enabled: !(state is LoginLoading),
                       obscureText: _obscureText,
-                      style: textTheme.body1.copyWith(
+                      style: textTheme.bodyText2.copyWith(
                         color: Colors.yellow[700],
                       ),
                       decoration: InputDecoration(
@@ -233,7 +248,7 @@ class _RegisterPageState extends State<RegisterPage> {
                           ),
                         ),
                         hintText: 'Password',
-                        hintStyle: textTheme.body1.copyWith(
+                        hintStyle: textTheme.bodyText2.copyWith(
                           color: Colors.black12,
                         ),
                       ),
@@ -245,7 +260,7 @@ class _RegisterPageState extends State<RegisterPage> {
                     child: TextField(
                       controller: nameController,
                       enabled: !(state is LoginLoading),
-                      style: textTheme.body1.copyWith(
+                      style: textTheme.bodyText2.copyWith(
                         color: Colors.yellow[700],
                       ),
                       decoration: InputDecoration(
@@ -262,7 +277,7 @@ class _RegisterPageState extends State<RegisterPage> {
                           ),
                         ),
                         hintText: 'Name',
-                        hintStyle: textTheme.body1.copyWith(
+                        hintStyle: textTheme.bodyText2.copyWith(
                           color: Colors.black12,
                         ),
                       ),
@@ -274,7 +289,7 @@ class _RegisterPageState extends State<RegisterPage> {
                     child: TextField(
                       controller: companyNameController,
                       enabled: !(state is LoginLoading),
-                      style: textTheme.body1.copyWith(
+                      style: textTheme.bodyText2.copyWith(
                         color: Colors.yellow[700],
                       ),
                       decoration: InputDecoration(
@@ -291,10 +306,93 @@ class _RegisterPageState extends State<RegisterPage> {
                           ),
                         ),
                         hintText: 'Company Name',
-                        hintStyle: textTheme.body1.copyWith(
+                        hintStyle: textTheme.bodyText2.copyWith(
                           color: Colors.black12,
                         ),
                       ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: DropdownButtonFormField(
+                      hint: Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                            child: Text(
+                              'Select Country',
+                              style: TextStyle(color: Colors.black),
+                            ),
+                          ),
+                        ],
+                      ),
+                      value: country == '' ? null : country,
+                      onChanged: (newValue) {
+                        setState(() {
+                          country = newValue;
+                          final index = countries.indexWhere(
+                                  (element) => element['iso3'] == country);
+                          countryStates.clear();
+                          countryStates = countries[index]['states'];
+                          countryState = "";
+                        });
+                      },
+                      items: countries.map((item) {
+                        return DropdownMenuItem(
+                          value: item['iso3'],
+                          child: Row(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                                child: Text(
+                                  item['name'],
+                                  style: TextStyle(color: Colors.black),
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      }).toList(),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: DropdownButtonFormField(
+                      hint: Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                            child: Text(
+                              'Select State',
+                              style: TextStyle(color: Colors.black),
+                            ),
+                          ),
+                        ],
+                      ),
+                      value: countryState == '' ? null : countryState,
+                      onChanged: (newValue) {
+                        setState(() {
+                          countryState = newValue;
+                        });
+                      },
+                      items: countryStates.map((item) {
+                        return DropdownMenuItem(
+                          value: item['state_code'],
+                          child: Row(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                                child: Text(
+                                  item['name'],
+                                  style: TextStyle(color: Colors.black),
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      }).toList(),
                     ),
                   ),
                   SizedBox(height: 50),
@@ -363,8 +461,6 @@ class _RegisterPageState extends State<RegisterPage> {
 class _LoginAppbar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
-    final textTheme = Theme.of(context).textTheme;
-
     return Container(
       color: Colors.white,
       child: Column(
